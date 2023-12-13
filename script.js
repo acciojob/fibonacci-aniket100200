937ed3b7eb2ff24720827aee85ff145719838628
Aniket Khangar <116418804+aniket100200@users.noreply.github.com>
2023-12-13 16:48:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-function fibonacci(num) {
-// your code here
+function fibonacci(num) 
+{
+	// your code here
+
+	let a=0;
+	let b=1;
+	if(num===1)return a;
+	if(num===2)return b;
+	for(let i=3;i<=num;i++){
+		let temp=a+b;
+		a=b;
+		b=temp;
+	}
+
+	return b;
+	
 }
 
 module.exports = fibonacci;
